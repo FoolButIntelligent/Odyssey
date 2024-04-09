ca3bfbee9fb1c4de656e00905de0d4ec2062f54d
YuHui <34625909+FoolButIntelligent@users.noreply.github.com>
2024-04-09 13:18:28 +0800

    feat : flamethower &  button

---
+++ Assets/Examples/Scripts/Misc/Toggle.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Toggle : MonoBehaviour
+{
+    public bool state = true;
+    public float delay;
+    public Toggle[] multiTrigger;
+
+    /// <summary>
+    /// Called when the Toggle is activated.
+    /// </summary>
+    public UnityEvent onActivate;
+
+    /// <summary>
+    /// Called when the Toggle is deactivated.
+    /// </summary>
+    public UnityEvent onDeactivate;
+
+    /// <summary>
+    /// Sets the state of the Toggle.
+    /// </summary>
+    /// <param name="value">The state you want to set.</param>
+    public virtual void Set(bool value)
+    {
+        StopAllCoroutines();
+        StartCoroutine(SetRoutine(value));
+    }
+
+    protected virtual IEnumerator SetRoutine(bool value)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (value)
+        {
+            if (!state)
+            {
+                state = true;
+
+                foreach (var toggle in multiTrigger)
+                {
+                    toggle.Set(state);
+                }
+
+                onActivate?.Invoke();
+            }
+        }
+        else if (state)
+        {
+            state = false;
+
+            foreach (var toggle in multiTrigger)
+            {
+                toggle.Set(state);
+            }
+
+            onDeactivate?.Invoke();
+        }
+    }
+}
